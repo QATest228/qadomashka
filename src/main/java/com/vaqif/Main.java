3c33fde34254b22b99e289d70adff59aaa9a4cf8
Vaqif Yusibli <vaqifyusibli@gmail.com>
2024-11-23 18:28:52 +0400

    Change message in master

--- src/main/java/com/vaqif/Main.java
+++ src/main/java/com/vaqif/Main.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 public class Main {
 
-    private static String msg = "Hello Vaqif111!";
+    private static String msg = "Hello master not conflict!";
 
     public static void main(String... args) {
         System.out.println(msg);
