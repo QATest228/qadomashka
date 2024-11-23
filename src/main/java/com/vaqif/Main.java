f57e54ffd1a4ebc59f0699ca33b0ebc2e75241fd
Vaqif Yusibli <vaqifyusibli@gmail.com>
2024-11-23 17:37:20 +0400

    105 na 107

--- src/main/java/com/vaqif/Main.java
+++ src/main/java/com/vaqif/Main.java
@@ -2,7 +2,7 @@ package guru.qa;
 
 public class Main {
 
-    private static String msg = "Hello Vaqif105!";
+    private static String msg = "Hello Vaqif107!";
 
     public static void main(String... args) {
         System.out.println(msg);
